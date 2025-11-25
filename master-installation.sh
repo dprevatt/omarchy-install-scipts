b894a7afa88f8434a810460dc67c2e54b5a5f08a
Dustin Prevatt <dprevatt27@gmail.com>
2025-11-25 00:06:59 -0600

    Adding initial scripts

---
+++ master-installation.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Bitwarden
+./apps/bitwarden-install.sh
+# Stow
+./apps/stow-install.sh
+# VSCode
+./apps/vscode-install.sh
+# Zen Browser
+./apps/zen-browser-install.sh
+# Tick-Tick
+./apps/tick-tick-install.sh
+# FreeTube
+./apps/freetube-install.sh
+# Warp-Terminal
+./apps/warp-terminal-install.sh
